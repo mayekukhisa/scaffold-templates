ef79d9bd9870d310732846dda45cb004acc56959
Mayeku Khisa <101919651+mayekukhisa@users.noreply.github.com>
2025-01-15 05:20:50 +0300

    feat: update `android` template to Compose BOM 2024.12.01

--- src/gradle/android/text/build.gradle.kts
+++ src/gradle/android/text/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 spotless {
   mapOf("ktlint_function_naming_ignore_when_annotated_with" to "Composable").let { config ->
-    with(rootProject.file("spotless/headers/kotlin.txt")) {
+    with(rootProject.file("spotless/kotlin-header.txt")) {
       kotlin {
         target("**/*.kt")
         targetExclude("**/build/**/*.kt")
@@ -30,12 +30,12 @@ spotless {
     target("**/*.xml")
     targetExclude("**/build/**/*.xml", ".idea/**/*.xml")
     eclipseWtp(EclipseWtpFormatterStep.XML).configFile(
-      rootProject.file("spotless/configs/xml.prefs"),
+      rootProject.file("spotless/xml-config.prefs"),
     )
-    licenseHeaderFile(rootProject.file("spotless/headers/xml.txt"), "^<[^!?]").updateYearWithLatest(true)
+    licenseHeaderFile(rootProject.file("spotless/xml-header.txt"), "^<[^!?]").updateYearWithLatest(true)
   }
 
-  with(rootProject.file("spotless/configs/prettierrc.json")) {
+  with(rootProject.file("spotless/prettier-config.json")) {
     json {
       target("**/*.json")
       targetExclude("**/build/**/*.json")
